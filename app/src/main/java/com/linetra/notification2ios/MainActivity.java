696d3b3ea7bb171476a129f8f0ba5fdfabca14b8
Vivek <vivek.oss@linetra.com>
2016-05-10 15:26:41 +0530

    Storing userid and token in shared preference.

--- app/src/main/java/com/linetra/notification2ios/MainActivity.java
+++ app/src/main/java/com/linetra/notification2ios/MainActivity.java
@@ -1,10 +1,12 @@
 package com.linetra.notification2ios;
 
+import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -16,16 +18,22 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
+    private Activity context = this;
     private static Button saveButton;
     private static EditText editText_userid;
     private static EditText editText_token;
     private static String userID;
     private static String token;
+    public static final String PREFS_NAME = "N2I_PREFS";
+    public static final String PREFS_USERID_KEY = "N2I_USERID";
+    public static final String PREFS_TOKEN_KEY = "N2I_TOKEN";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,22 +60,63 @@ public class MainActivity extends AppCompatActivity {
         saveButton = (Button) findViewById(R.id.save_button);
         editText_userid = (EditText) findViewById(R.id.editText1);
         editText_token = (EditText) findViewById(R.id.editText2);
+
+        get_prefs(context);
+        if (userID != null && !userID.isEmpty()) {
+            editText_userid.setText(userID);
+            editText_userid.setCursorVisible(false);
+
+        }
+        if (token != null && !token.isEmpty()) {
+            editText_token.setText(token);
+            editText_token.setCursorVisible(false);
+        }
+
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (editText_userid.getText() == null || editText_token.getText() == null)
+                if (editText_userid.getText().toString() == null || editText_token.getText().toString() == null ||
+                        editText_userid.getText().toString().isEmpty() || editText_token.getText().toString().isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Invalid UserID/Token ",
+                            Toast.LENGTH_LONG).show();
                     Log.d(TAG, "Invalid UserID / Token");
+                }
                 else {
                     editText_token.setCursorVisible(false);
-                    editText_token.setCursorVisible(false);
+                    editText_userid.setCursorVisible(false);
                     userID = editText_userid.getText().toString();
                     token = editText_token.getText().toString();
                     Log.d(TAG, "UserID: " +userID);
                     Log.d(TAG, "Token: " +token);
-                }
+                    save_prefs(context, userID, token);
+                    Toast.makeText(MainActivity.this, "UserID & Token saved",
+                            Toast.LENGTH_LONG).show();
 
+                }
             }
         });
+
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+
+    }
+
+    public void get_prefs(Context context) {
+        SharedPreferences settings;
+        String text;
+        settings = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+        userID = settings.getString(PREFS_USERID_KEY, null);
+        token = settings.getString(PREFS_USERID_KEY, null);
+    }
+
+    public void save_prefs(Context context, String userid, String token) {
+        SharedPreferences settings;
+        SharedPreferences.Editor editor;
+        settings = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+        editor = settings.edit();
+
+        editor.putString(PREFS_USERID_KEY, userid);
+        editor.putString(PREFS_TOKEN_KEY, token);
+        editor.commit();
     }
 
     private static IntentFilter makeIntentFilter() {
